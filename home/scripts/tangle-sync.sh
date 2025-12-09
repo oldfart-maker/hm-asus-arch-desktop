f92d0057f38eb1704d193e132ef2c8e56e70250b
Mike Burns <mkburns61@yahoo.com>
2025-12-09 09:44:54 -0500

    changed repo name

--- home/scripts/tangle-sync.sh
+++ home/scripts/tangle-sync.sh
@@ -22,7 +22,7 @@
 set -Eeuo pipefail
 
 # --- defaults -------------------------------------------------------------
-REPO="${REPO:-$HOME/projects/hm-dell-arch-laptop}"
+REPO="${REPO:-$HOME/projects/hm-asus-arch-desktop}"
 
 NIRI_SRC_DIR="${NIRI_SRC_DIR:-$HOME/projects/niri_babel_config}"
 EMACS_SRC_DIR="${EMACS_SRC_DIR:-$HOME/projects/emacs_babel_config}"
