8fc70a50679384d28f40869a1cdd93377306172e
Mike Burns <mkburns61@yahoo.com>
2025-12-28 15:44:24 -0500

    added config file copy

--- home/modules/apps/dankmaterialshell.nix
+++ home/modules/apps/dankmaterialshell.nix
@@ -1,12 +1,22 @@
-{ inputs, ... }:
+{ inputs, repoPath, ... }:
+
+let
+  settingsJson =
+    repoPath "home/data/apps/dankmaterialshell/settings.json";
+in
 {
   imports = [
-    inputs.dms.homeModules.dankMaterialShell.default
+    inputs.dms.homeModules.default
   ];
 
   programs.dank-material-shell = {
     enable = true;
     systemd.enable = true;
-    enableSystemMonitoring=false;
+
+    # nixpkgs on Arch doesn't have dgop
+    enableSystemMonitoring = false;
   };
+
+  home.file.".config/DankMaterialShell/settings.json".source =
+    settingsJson;
 }
