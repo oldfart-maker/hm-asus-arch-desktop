7cf01077fa35ebbed9eb9823dda6116cf104b3c4
Mike Burns <mkburns61@yahoo.com>
2025-12-28 18:32:11 -0500

    force to a newer version of matugen

--- home/modules/apps/dankmaterialshell.nix
+++ home/modules/apps/dankmaterialshell.nix
@@ -1,27 +1,13 @@
 { inputs, pkgs, repoPath, ... }:
 
 let
-  settingsJson = repoPath "home/data/apps/dankmaterialshell/settings.json";
+  settingsJson =
+    repoPath "home/data/apps/dankmaterialshell/settings.json";
 
   dmsShellAssets =
     "${inputs.dms.packages.${pkgs.system}.dms-shell}/share/quickshell/dms";
-
-  matugenForDms = pkgs.matugen.overrideAttrs (old: {
-    src = pkgs.fetchFromGitHub {
-      owner = "InioX";
-      repo  = "matugen";
-      rev   = "v2.4.1";
-      hash  = "sha256-USsStRd1J+yjtReWwXt8ZEnzLAp3qM/XkfVknjftd2k=";
-      cargoHash = "";          
-    };     
-  });
-    
 in
 {
-  home.packages = [
-    matugenForDms
-  ];
-  
   imports = [
     inputs.dms.homeModules.default
   ];
@@ -32,6 +18,18 @@ in
     enableSystemMonitoring = false;
   };
 
-  home.file.".config/DankMaterialShell/settings.json".source = settingsJson;
-  home.file.".config/quickshell/dms".source = dmsShellAssets;
+  # Force DMS to use system matugen (3.1.0 from pacman)
+  systemd.user.services.dms = {
+    Service = {
+      Environment = [
+        "PATH=/usr/bin:/run/current-system/sw/bin"
+      ];
+    };
+  };
+
+  home.file.".config/DankMaterialShell/settings.json".source =
+    settingsJson;
+
+  home.file.".config/quickshell/dms".source =
+    dmsShellAssets;
 }
