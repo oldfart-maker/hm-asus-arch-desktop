b99c47a86b66a09b05ade3832a7b83f6b1e6dbe0
Mike Burns <mkburns61@yahoo.com>
2025-12-28 19:33:37 -0500

    revert back to original

--- home/modules/apps/dankmaterialshell.nix
+++ home/modules/apps/dankmaterialshell.nix
@@ -1,35 +1,37 @@
-{ inputs, pkgs, repoPath, ... }:
+{ pkgs, ... }:
 
 let
-  settingsJson =
-    repoPath "home/data/apps/dankmaterialshell/settings.json";
+  # Upstream installer archive (gzipped)
+  dankInstallUrl =
+    "https://github.com/AvengeMedia/DankMaterialShell/releases/download/v0.5.1/dankinstall-amd64.gz";
 
-  dmsShellAssets =
-    "${inputs.dms.packages.${pkgs.system}.dms-shell}/share/quickshell/dms";
+  # Use the sha256 published by the project (or from nix-prefetch-url)
+  # Example placeholder:
+  dankInstallSha256 = "sha256:e017b2b0df7b720bf12661bbdbaba44eac61d7c09797a671f40c4f619ec59ae9";
 in
 {
-  imports = [
-    inputs.dms.homeModules.default
+  home.packages = [
+    (pkgs.runCommand "dankmaterialshell" { buildInputs = [ pkgs.gzip ]; } ''
+      mkdir -p $out/bin
+
+      # Download the gzipped installer/binary
+      installer_gz=${pkgs.fetchurl {
+        url = dankInstallUrl;
+        sha256 = dankInstallSha256;
+      }}
+
+      # Decompress into $out/bin/dankmaterialshell-installer (or binary)
+      gzip -c -d "$installer_gz" > $out/bin/dankmaterialshell-installer
+
+      chmod +x $out/bin/dankmaterialshell-installer
+
+      # Create a user-facing wrapper called `dankmaterialshell`
+      cat > $out/bin/dankmaterialshell << 'EOF'
+      #!/usr/bin/env bash
+      exec "$(dirname "$0")/dankmaterialshell-installer" "$@"
+      EOF
+
+      chmod +x $out/bin/dankmaterialshell
+    '')
   ];
-
-  programs.dank-material-shell = {
-    enable = true;
-    systemd.enable = true;
-    enableSystemMonitoring = false;
-  };
-
-  # Force DMS to use system matugen (3.1.0 from pacman)
-#  systemd.user.services.dms = {
-#    Service = {
-#      Environment = [
-#        "PATH=/usr/bin:/run/current-system/sw/bin"
-#      ];
-#    };
-#  };
-
-#  home.file.".config/DankMaterialShell/settings.json".source =
-#    settingsJson;
-
-#  home.file.".config/quickshell/dms".source =
-#    dmsShellAssets;
 }
