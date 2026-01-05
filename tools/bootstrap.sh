a17f70e266b0f9b4c388aa36b7c58e83ac69ed05
Mike Burns <mkburns61@yahoo.com>
2026-01-05 12:17:37 -0500

    removed virt-manager from install

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -309,7 +309,7 @@ setup_virt_in_target() {
   local TARGET_USER="username"
 
   arch-chroot "$TARGET_MNT" pacman --noconfirm -S --needed \
-    qemu-base libvirt virt-manager dnsmasq  bridge-utils
+    qemu- libvirt  dnsmasq  bridge-utils
 
   # Sanity: user must exist in target
   if ! arch-chroot "$TARGET_MNT" id "$TARGET_USER" >/dev/null 2>&1; then
