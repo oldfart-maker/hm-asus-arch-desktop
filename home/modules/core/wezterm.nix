69061e172cbd0dfe835aa81148d7357489fd4b9d
oldfart-maker <mkburns61@yahoo.com>
2026-01-09 17:45:49 -0500

    added keybindings.lua

--- home/modules/core/wezterm.nix
+++ home/modules/core/wezterm.nix
@@ -2,14 +2,12 @@
 
 let
   cfgPath  = repoPath "home/data/apps/wezterm/wezterm.lua";
+  keybindingsPath = repoPath "home/data/apps/wezterm/keybindings.lua";
   colorsPath = repoPath "home/data/apps/wezterm/colors";
 
   # Wrap wezterm to use nixGL
   weztermWrapped = pkgs.writeShellScriptBin "wezterm" ''
-    export WAYLAND_DISPLAY="wayland-0"
     export WINIT_UNIX_BACKEND=wayland
-
-    # If you have a specific nixGL variant, use it here
     exec nixGL "${pkgs.wezterm}/bin/wezterm" "$@"
   '';  
 in
@@ -21,6 +19,11 @@ in
 
   xdg.configFile."wezterm/wezterm.lua" = {
     source = cfgPath;
+    force  = true;
+  };
+
+  xdg.configFile."wezterm/keybindings.lua" = {
+    source = keybindingsPath;
     force  = true;
   };
 
